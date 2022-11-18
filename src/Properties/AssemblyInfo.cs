44d299a8670578bcbaa0f93752eadc90996e5b90
Paige Seivertson <pzeig66@yahoo.com>
2022-11-18 10:48:44 -0500

    Update copyright year

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SteamCMD-GUI")]
-[assembly: AssemblyCopyright("Copyright © Paige Seivertson 2020")]
+[assembly: AssemblyCopyright("Copyright © Paige Seivertson 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
